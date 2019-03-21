62c7c51dd7e810f7b540f1c648964184ce90e38a
TomaszCz <tomasz.m.czajka@gmail.com>
2019-03-21 18:49:11 +0100

    rename few stuff

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# FingoChallenge
+# CustomDropdown
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.5.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to fingo-challenge!');
+    expect(page.getTitleText()).toEqual('Welcome to custom-dropdown!');
   });
 
   afterEach(async () => {
